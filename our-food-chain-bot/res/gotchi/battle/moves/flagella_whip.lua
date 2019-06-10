8bbbfff0cbd30f417cdf84c95055724f54e32755
gsemac <gabe.semac@gmail.com>
2019-06-09 21:14:15 -0600

    Added additional gotchi moves

---
+++ our-food-chain-bot/res/gotchi/battle/moves/flagella_whip.lua
@@ -0,0 +1,15 @@
+﻿function register(move)
+
+	move.name = "flagella whip";
+	move.description = "Whips the opponent with flagella, dealing consistent base damage.";
+
+	move.pp = 20;
+	move.type = type.Offensive;
+
+	move.requires.match = "flagella|flagellum";
+
+end
+
+function callback(args) 
+	args.DoDamage(3);
+end

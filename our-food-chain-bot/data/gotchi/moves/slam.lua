c0c35d88bc0bd975c153de4c0714c12fe9a0de70
Disgustedorite <cnax1998@gmail.com>
2019-06-15 14:25:22 -0500

    "Slam" attack
    This is a low-priority, high-damage attack for gotchis which have heavy appendages.

---
+++ our-food-chain-bot/data/gotchi/moves/slam.lua
@@ -0,0 +1,16 @@
+function register(move)
+	move.name = "slam"
+	move.description = "Strikes the opponent with a heavy appendage. This move deals a lot of damage, but always strikes last."
+	
+	move.pp = 5
+	move.type = type.Offensive
+	move.multiplier = 2.0
+	move.priority = -10
+	
+	move.requires.minlevel = 30
+	move.requires.match = "heavy tail|heavy limb|heavy appendage|thick tail|thick appendage"
+end
+
+function callback(args)
+	args.DoDamage()
+end

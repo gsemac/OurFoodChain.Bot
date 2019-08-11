0cdaeb04df2314bd27a2015a1606460eccacbe56
gsemac <gabe.semac@gmail.com>
2019-08-11 12:09:57 -0600

    Fixed issue with ConjunctiveJoin method

--- our-food-chain-bot/utils/StringUtils.cs
+++ our-food-chain-bot/utils/StringUtils.cs
@@ -55,7 +55,7 @@ namespace OurFoodChain {
             return _joinWithUniqueEndSeparator(separator, " or ", values);
         }
         public static string ConjunctiveJoin(string separator, IEnumerable<string> values) {
-            return _joinWithUniqueEndSeparator(separator, separator.EndsWith(" ") ? "and " : " and ", values);
+            return _joinWithUniqueEndSeparator(separator, separator.EndsWith(" ") && values.Count() > 2 ? "and " : " and ", values);
         }
         public static string GetFirstSentence(string value) {
 

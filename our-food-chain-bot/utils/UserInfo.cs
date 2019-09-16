7572200f08f9773f0c9a384e927040889110dfa7
gsemac <gabe.semac@gmail.com>
2019-09-16 14:50:11 -0600

    Removed "setowner" aliases; "setowner" command will set user ID if the user is not present but has been seen before

---
+++ our-food-chain-bot/utils/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFoodChain {
+
+    public class UserInfo {
+
+        public string Username { get; set; }
+        public ulong UserId { get; set; } = 0;
+
+    }
+
+}

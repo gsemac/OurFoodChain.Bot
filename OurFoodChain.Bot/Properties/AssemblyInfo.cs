7ed4f9803983406b89b37d5499bf1751c063dfc4
gsemac <gabe.semac@gmail.com>
2020-04-01 19:11:58 -0600

    Changed assembly info

--- OurFoodChain.Bot/Properties/AssemblyInfo.cs
+++ OurFoodChain.Bot/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("our-food-chain-bot")]
+[assembly: AssemblyTitle("OurFoodChain.Bot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("our-food-chain-bot")]
+[assembly: AssemblyProduct("OurFoodChain.Bot")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
